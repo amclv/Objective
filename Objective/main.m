0b0e01281a0f32992ef2837cd5fd306475e746bc
Aaron Cleveland <toscleveland@gmail.com>
2020-03-16 11:12:28 -0500

    init

---
+++ Objective/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Objective
+//
+//  Created by Aaron Cleveland on 3/16/20.
+//  Copyright © 2020 Aaron Cleveland. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
